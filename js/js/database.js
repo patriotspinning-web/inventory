00b950ed7cf6fb9ef2a2a28afd93f6e1c0894377
patriotspinning-web <patriotspinning@gmail.com>
2026-07-31 17:50:04 +0600

    Update database.js

--- js/js/database.js
+++ js/js/database.js
@@ -1,1 +1,20 @@
+import {supabase}
+from "./supabase.js";
 
+export async function addCottonReceive(data){
+
+const {error}=await supabase
+.from("cotton_receive")
+.insert([data]);
+
+if(error){
+
+console.error(error);
+
+}else{
+
+alert("Saved Successfully");
+
+}
+
+}
