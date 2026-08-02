a69a094e7afae645a332d04e6cd1b9c5c9ed6a3d
patriotspinning-web <patriotspinning@gmail.com>
2026-08-02 19:45:17 +0600

    Update database.js

--- js/database.js
+++ js/database.js
@@ -1,1 +1,14 @@
+import { supabase } from "./supabase.js";
 
+export async function addCottonReceive(data) {
+    const { error } = await supabase
+        .from("cotton_receive")
+        .insert([data]);
+
+    if (error) {
+        console.error(error);
+        return false;
+    }
+
+    return true;
+}
